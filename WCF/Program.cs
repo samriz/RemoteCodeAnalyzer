433ec88ab2c48a93c073e2a14c1f30adaec8d725
samriz <samriz@users.noreply.github.com>
2021-03-08 15:00:36 -0600

    Create separate Console Application called "WCF" and placed Server.cs in there. Also created Service.cs, which will implement the functions declared in Server.cs

--- WCF/Program.cs
+++ WCF/Program.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ServiceModel;
 
-namespace RemoteCodeAnalyzer
+namespace WCF
 {
-    class Server
+    public class Service
     {
+        /*static void Main(string[] args)
+        {
+        }*/
     }
 }
---
+++ WCF/Server.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Xml;
+
+namespace RemoteCodeAnalyzer
+{
+    [ServiceContract]
+    public interface IBasicService
+    {
+        //clients have to use this interface to communicate with server
+        [OperationContract]
+        void sendMessage(string message);
+        [OperationContract]
+        void sendMessage(XmlDocument xmlMessage);
+        [OperationContract] //exposed to client
+        void Login();
+        [OperationContract]
+        void UploadFiles();
+    }
+
+    [MessageContract]
+    public class RemoteFileInfo : IDisposable
+    {
+
+    }
+    class Server
+    {
+    }
+}
