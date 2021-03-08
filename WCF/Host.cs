892bb6a3b92e1a23768b54655107911e2ad39f26
samriz <samriz@users.noreply.github.com>
2021-03-08 15:08:17 -0600

    Create Host.cs

---
+++ WCF/Host.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF
+{
+    class Host
+    {
+        //define endpoints and open them for the connections
+    }
+}
